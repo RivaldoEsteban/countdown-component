dfca1d2ddce409672c62a0a7f7b4ef1b343ad003
Rivaldo Esteban <63760024+RivaldoEsteban@users.noreply.github.com>
2021-06-10 13:01:02 -0500

    contador

--- js.js
+++ js.js
@@ -1,73 +1,68 @@
-// fecha actual
-// const birthday = new Date("08 28 2021 19:10:17 GMT-0500 ");
-// const coutdown = birthday - date;
-// console.log(coutdown / 1000); //segundos
-// console.log(coutdown / 1000 / 60); //minutos
-// console.log(coutdown / 1000 / 60 / 60); //horas
-// console.log(coutdown / 1000 / 60 / 60 / 24); //dias
+function countdownToMyBirthday(maximumDate) {
+  const date = new Date();
+  const remainTime = (new Date(maximumDate) - date + 1000) / 1000; //segundos
+  const seconds = Math.floor(remainTime % 60)
+    .toString()
+    .padStart(2, "00");
+  const minutes = Math.floor((remainTime / 60) % 60)
+    .toString()
+    .padStart(2, "00");
+  const hours = Math.floor((remainTime / 3600) % 24)
+    .toString()
+    .padStart(2, "00");
+  const days = Math.floor(remainTime / (3600 * 24))
+    .toString()
+    .padStart(2, "00");
+
+  return {
+    remainTime,
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+}
 
 const day = document.getElementById("dias");
 const hour = document.getElementById("horas");
 const minute = document.getElementById("minutos");
-const seconds = document.getElementById("segundos");
+const second = document.getElementById("segundos");
 
-// console.log(countdownToMyBirthday("08 28 2021 19:10:17 GMT-0500"));
-
-const container = document.getElementById("countdown");
-
-const countdown = (maximumDate, elem, finalMessage) => {
-  const el = document.getElementById(elem);
+const countdown = (maximumDate, finalMessage) => {
+  const container = document.getElementById("countdown");
   const timerUpdate = setInterval(() => {
-    const date = new Date();
-    function countdownToMyBirthday(maximumDate) {
-      const remainTime = (new Date(maximumDate) - date + 1000) / 1000; //segundos
-      const seconds = Math.floor(remainTime % 60)
-        .toString()
-        .padStart(2, "00");
-      const minutes = Math.floor((remainTime / 60) % 60)
-        .toString()
-        .padStart(2, "00");
-      const hours = Math.floor((remainTime / 3600) % 24)
-        .toString()
-        .padStart(2, "00");
-      const days = Math.floor(remainTime / (3600 * 24))
-        .toString()
-        .padStart(2, "00");
-
-      return {
-        remainTime,
-        seconds,
-        minutes,
-        hours,
-        days,
-      };
-    }
-    let t = countdownToMyBirthday(maximumDate);
-    el.innerHTML = `
-      <div>
-        <p class="number" id="dias">${t.days}</p>
-        <p class="txt">Días</p>
-      </div>
-      <div>
-        <p class="number" id="horas">${t.hours}</p>
-        <p class="txt">Horas</p>
-      </div>
-      <div>
-        <p class="number" id="minutos">${t.minutes}</p>
-        <p class="txt">Minutos</p>
-      </div>
-      <div>
-        <p class="number" id='segundos'>${t.seconds}</p>
-        <p class="txt">Segundos</p>
-      </div>
-    `;
-    if (t.remainTime <= 1) {
-      countdown("08 28 2021 19:10:17 GMT-0500", "countdown");
-      el.innerHTML = finalMessage;
+    const time = countdownToMyBirthday(maximumDate);
+    day.textContent = time.days;
+    hour.textContent = time.hours;
+    minute.textContent = time.minutes;
+    second.textContent = time.seconds;
+    if (time.remainTime <= 1) {
+      // container.innerHTML = finalMessage;
+      return countdown("08 28 2021 19:10:17 GMT-0500", "countdown");
     }
   }, 1000);
 };
 
-const cumplePau = "";
+//formulario
+const form = document.getElementById("form");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
 
-countdown("10 28 2021 00:00:00 GMT-0500", "countdown", "tiempo finalizado");
+button.addEventListener("click", (event) => {
+  event.preventDefault();
+  const date = new Date();
+  const x = `${input.value}  ${date
+    .getHours()
+    .toString()
+    .padStart(2, "00")}:${date.getMinutes().toString().padStart(2, "00")}:${date
+    .getSeconds()
+    .toString()
+    .padStart(2, "00")} GMT-0500`;
+  countdown(x, "tiempo finalizado"); //fecha , contenedor, mensaje
+});
+
+const cumplePaula = "10 28 2021 00:00:00 GMT-0500";
+const cumpleRivaldo = "08 28 2021 00:00:00 GMT-0500";
+const cumpleLeo = "12 08 2021 00:00:00 GMT-0500";
+
+// countdown(cumpleRivaldo, "countdown", "tiempo finalizado");
